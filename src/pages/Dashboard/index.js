2e897b881d56b1368919f6b15b7dc6fe0b57063d
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-18 15:42:40 -0300

    Lista de agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import { Container, Title, List } from './styles';
+import Appointment from '~/components/Appointment';
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Dashboard() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={data}
+          keyExtractor={(item) => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 Dashboard.navigationOptions = {
