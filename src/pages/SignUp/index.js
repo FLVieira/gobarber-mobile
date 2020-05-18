eac34cde5cd15ae873007520059a408a4d4097fe
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-18 14:27:28 -0300

    Conectando o Redux

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.png';
-
 import Background from '~/components/Background';
+
+import * as authActions from '~/store/modules/auth/actions';
 
 import {
   Container,
@@ -15,10 +17,20 @@ import {
 } from './styles';
 
 export default function SignUp({ navigation }) {
+  const dispatch = useDispatch();
+
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector((state) => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(authActions.signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -32,6 +44,8 @@ export default function SignUp({ navigation }) {
             autoCapitalize="none"
             placeholder="Nome Completo"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
           <FormInput
             icon="mail-outline"
@@ -41,6 +55,8 @@ export default function SignUp({ navigation }) {
             placeholder="Digite o seu e-mail"
             ref={emailRef}
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -49,8 +65,12 @@ export default function SignUp({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
-          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Criar conta
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignIn')}>
