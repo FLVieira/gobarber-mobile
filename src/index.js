361bce51e75f8759a6e712d7e48a1fcee0c7a9d0
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-18 01:54:19 -0300

    Reactotron

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
-
+import './config/ReactotronConfig';
 import Routes from './routes';
 
 export default function App() {
